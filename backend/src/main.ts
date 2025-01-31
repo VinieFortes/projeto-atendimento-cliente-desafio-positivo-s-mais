f5ca5f1950fd83e3bb1a8cd5d687521f51f7ca2e
VinieFortes <vinie4apps@gmail.com>
2025-01-31 20:05:59 -0300

    ajustes para build

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // URL do seu frontend Angular
+    origin: [
+        'http://localhost:4200',
+        'https://projeto-atendimento-cliente-desafio-wctkx.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Se você estiver usando cookies ou autenticação baseada em sessão
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
