aa0582d0fd80bfa9c8b72e8ddc0218bcc5f0d9ed
VinieFortes <vinie4apps@gmail.com>
2025-02-01 09:38:58 -0300

    correções de css

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, HostListener, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatFabButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
-import {NgIf} from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
 import { Router } from '@angular/router';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {UserComponent} from './auth/user/user.component';
@@ -10,7 +10,7 @@ import {UserComponent} from './auth/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatFabButton, MatIcon, NgIf, MatMenu, MatMenuItem, MatIconButton, MatMenuTrigger, UserComponent],
+  imports: [RouterOutlet, MatFabButton, MatIcon, NgIf, MatMenu, MatMenuItem, MatIconButton, MatMenuTrigger, UserComponent, NgClass],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
